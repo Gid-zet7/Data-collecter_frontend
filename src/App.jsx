2f9891d02757fe6b2a617034e0bd9936127bc1ba
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-12-08 17:56:40 +0000

    Added features routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import DashboardLayout from "./components/DashboardLayout";
+import Public from "./components/Public";
+import Login from "./features/auth/Login";
+import Signup from "./features/auth/Signup";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />} />
+      <Route index element={<Public />} />
+      <Route path="login" element={<Login />} />
+      <Route path="signup" element={<Signup />} />
       <Route path="dashboard" element={<DashboardLayout />} />
     </Routes>
   );
