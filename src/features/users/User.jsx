a36139f93b76a6fe0437227204c2425a5320af15
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-12-09 06:04:59 +0000

    The user component finds and diaplays a specific user with the provided id

---
+++ src/features/users/User.jsx
@@ -0,0 +1,43 @@
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/prop-types */
+import { useNavigate } from "react-router-dom";
+import { useGetUsersQuery } from "./usersApiSlice";
+import { memo } from "react";
+import { Link } from "react-router-dom";
+
+const User = ({ userId }) => {
+  // Destructure the user from result
+  const { user } = useGetUsersQuery("usersList", {
+    selectFromResult: ({ data }) => ({
+      user: data?.entities[userId],
+    }),
+  });
+
+  const navigate = useNavigate();
+
+  if (user) {
+    const handleEdit = () => navigate(`/dash/users/${userId}`);
+
+    const userRolesString = user.roles.toString().replaceAll(",", ", ");
+
+    const cellStatus = user.active ? "" : "table__cell--inactive";
+
+    return (
+      <tr>
+        <td className={`table__cell ${cellStatus}`}>
+          <Link to={`/dash/users/${user.id}`}>{user.username} </Link>
+        </td>
+        <td className={`table__cell ${cellStatus}`}>{userRolesString}</td>
+        <td className={`table__cell ${cellStatus}`}>
+          <button className="btn" onClick={handleEdit}>
+            Edit
+          </button>
+        </td>
+      </tr>
+    );
+  } else return null;
+};
+
+const memoizedUser = memo(User);
+
+export default memoizedUser;
