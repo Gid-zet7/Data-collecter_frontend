3167806a2f376e4833c6b98974ab981a71aefe6e
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-12-09 03:16:32 +0000

    Added some comments to the apiSlice

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -1,9 +1,14 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-// import { CreateApi } from "@reduxjs/toolkit/query";
 
+// Creating the api
 export const apiSlice = createApi({
+  // Set base url to fetch or perform operations on data to the backend
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8080" }),
+
+  // Tag types will be used for cached data
   tagTypes: ["Data Form", "Migrant", "User"],
+
+  //   We will attach extended slices for data form, migrant, and user to this api slice
   // eslint-disable-next-line no-unused-vars
   endpoints: (builder) => ({}),
 });
