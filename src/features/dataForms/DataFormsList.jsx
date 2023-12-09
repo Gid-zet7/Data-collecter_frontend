d7020f91eaa30941c245187cacbcb8b456055fd8
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-12-09 07:09:09 +0000

    Modified dataformsList component to display all migrant data forms  on the page

--- src/features/dataForms/DataFormsList.jsx
+++ src/features/dataForms/DataFormsList.jsx
@@ -1,4 +1,59 @@
+import { useGetDataFormsQuery } from "./dataFormsApiSlice";
+import { PulseLoader } from "react-spinners";
+import DataForm from "./DataForm";
+
 const DataFormsList = () => {
-  return <h1>DataFormsList</h1>;
+  const {
+    data: dataforms, //we rename the data using destructuring
+    //We have several state that we can monitor here
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+    // useGetDataFormsQuery is the hook that RTK query automatically created for us
+  } = useGetDataFormsQuery(undefined, {
+    pollingInterval: 60000,
+    refetchOnFocus: true,
+    refetchOnMountOrArgChange: true,
+  });
+
+  let content;
+
+  // We check if isLoading is true then we set the content to a pulse loader
+  if (isLoading)
+    content = (
+      <div className="loader-container">
+        <PulseLoader color={"#000"} className="pulse-loader" />
+      </div>
+    );
+
+  // If isError is true then we set the content to display the error message
+  if (isError) {
+    content = <p className="errmsg">{error?.data?.message} </p>;
+  }
+
+  // If isSuccess is true then we set the content to display dataforms
+  if (isSuccess) {
+    // Destructure ids from the data(now called dataforms)
+    const { ids } = dataforms;
+
+    const divContent =
+      // Provide the ids to the DataForm component
+      ids?.length &&
+      ids.map((dataformId) => (
+        <DataForm key={dataformId} dataformId={dataformId} />
+      ));
+
+    content = (
+      <div
+        className="div div--dataforms"
+        style={{ marginTop: "9rem", padding: "2rem" }}
+      >
+        {divContent}
+      </div>
+    );
+  }
+  return content;
 };
+
 export default DataFormsList;
