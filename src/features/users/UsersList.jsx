b3315a9ca9781a6299376f9ab9b8a9d18d5ad52a
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-12-09 06:03:16 +0000

    Modified usersList component to display all users on the usersList page

--- src/features/users/UsersList.jsx
+++ src/features/users/UsersList.jsx
@@ -1,4 +1,63 @@
+import { useGetUsersQuery } from "./usersApiSlice";
+import { PulseLoader } from "react-spinners";
+import User from "./User";
+
 const UsersList = () => {
-  return <h1>UsersList</h1>;
+  const {
+    data: users, //we rename the data using destructuring
+    //We have several state that we can monitor here
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+    // useGetUsersQuery is the hook that RTK query automatically created for us
+  } = useGetUsersQuery(undefined, {
+    pollingInterval: 60000,
+    refetchOnFocus: true,
+    refetchOnMountOrArgChange: true,
+  });
+
+  let content;
+
+  // We check if isLoading is true then we set the content to a pulse loader
+  if (isLoading)
+    content = (
+      <div className="loader-container">
+        <PulseLoader color={"#000"} className="pulse-loader" />
+      </div>
+    );
+
+  // If isError is true then we set the content to display the error message
+  if (isError) {
+    content = <p className="errmsg">{error?.data?.message} </p>;
+  }
+
+  // If isSuccess is true then we set the content to display users
+  if (isSuccess) {
+    // Destructure ids from the data(now called users)
+    const { ids } = users;
+
+    const tableContent =
+      // Provide the ids to the User component
+      ids?.length && ids.map((userId) => <User key={userId} userId={userId} />);
+
+    content = (
+      <table
+        className="table table--users"
+        style={{ marginTop: "9rem", padding: "2rem" }}
+      >
+        <thead>
+          <tr>
+            <th>Username</th>
+            <th>Roles</th>
+            <th>Edit</th>
+          </tr>
+        </thead>
+        <tbody>{tableContent}</tbody>
+      </table>
+    );
+  }
+  return content;
 };
+
 export default UsersList;
