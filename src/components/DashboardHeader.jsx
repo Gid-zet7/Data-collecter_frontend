2c14a885a23a3127507c2241dc5bb7aab43b6606
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-12-11 10:53:31 +0000

    Modified the dash board header to test routing to other pages

--- src/components/DashboardHeader.jsx
+++ src/components/DashboardHeader.jsx
@@ -1,5 +1,22 @@
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
 const DashboardHeader = () => {
-  return <h1>Header</h1>;
+  const navigate = useNavigate();
+
+  const onNewDataFormClicked = () => navigate("/dashboard/dataforms/addform");
+  const onNewUserClicked = () => navigate("/dashboard/users/adduser");
+  const onNewMigrantClicked = () => navigate("/dashboard/migrants/addmigrant");
+  const onResearchFormsClicked = () => navigate("/dashboard/dataforms");
+
+  return (
+    <>
+      <Button onClick={onNewDataFormClicked}>New Question form</Button>
+      <Button onClick={onNewUserClicked}>New User form</Button>
+      <Button onClick={onNewMigrantClicked}>New Migrant form</Button>
+      <Button onClick={onResearchFormsClicked}>Research forms</Button>
+    </>
+  );
 };
 
 export default DashboardHeader;
