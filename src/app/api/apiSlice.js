babb663bb4e833bdd6b070eba03f934ef00765f0
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-12-12 17:37:10 +0000

    Modified apiSlice

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -1,50 +1,55 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-// import { setCredentials } from "../../features/auth/authSlice";
+import { setCredentials } from "../../features/auth/authSlice";
 
-// const baseQuery = fetchBaseQuery({
-//   baseUrl: "http://localhost:8080",
-//   credentials: "include",
-//   prepareHeaders: (headers, { getState }) => {
-//     const token = getState().auth.token;
-//     if (token) {
-//       headers.set("authorization", `Bearer ${token}`);
-//     }
-//     return headers;
-//   },
-// });
+const baseQuery = fetchBaseQuery({
+  baseUrl: "http://localhost:8080",
+  // ensures our cookie is sent
+  credentials: "include",
+  // We prepare our headers
+  prepareHeaders: (headers, { getState }) => {
+    // we destructure getState and use it to get the the current state of our app and retrieve the current token
+    const token = getState().auth.token;
 
-// const baseQueryWithReauth = async (args, api, extraOptions) => {
-//   // console.log(`args--${args}`);
-//   // console.log(`api--${api}`);
-//   // console.log(`extraOptions--${extraOptions}`); // {shout: true}
+    // We set authorization header with the retrieved token
+    if (token) {
+      headers.set("authorization", `Bearer ${token}`);
+    }
+    return headers;
+  },
+  // This is applied to every request we send
+});
 
-//   let result = await baseQuery(args, api, extraOptions);
+const baseQueryWithReauth = async (args, api, extraOptions) => {
+  // console.log(`args--${args}`); //request url, method, body
+  // console.log(`api--${api}`); //signal, dispatch, getState()
+  // console.log(`extraOptions--${extraOptions}`); // {shout: true}
+  let result = await baseQuery(args, api, extraOptions);
 
-//   if (result?.error?.status === 403) {
-//     console.log("sending refresh token");
+  if (result?.error?.status === 403) {
+    console.log("sending refresh token");
 
-//     // send refresh token to get new access token
-//     const refreshResult = await baseQuery("/auth/refresh", api, extraOptions);
+    // send refresh token to get new access token
+    const refreshResult = await baseQuery("/auth/refresh", api, extraOptions);
 
-//     if (refreshResult?.data) {
-//       // store the new token
-//       api.dispatch(setCredentials({ ...refreshResult.data }));
+    if (refreshResult?.data) {
+      // store the new token
+      api.dispatch(setCredentials({ ...refreshResult.data }));
 
-//       result = await baseQuery(args, api, extraOptions);
-//     } else {
-//       if (refreshResult?.error?.status === 403) {
-//         refreshResult.error.data.message = "Your login has expired";
-//       }
-//       return refreshResult;
-//     }
-//   }
-//   return result;
-// };
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      if (refreshResult?.error?.status === 403) {
+        refreshResult.error.data.message = "Your login has expired";
+      }
+      return refreshResult;
+    }
+  }
+  return result;
+};
 
 // Creating the api
 export const apiSlice = createApi({
   // Set base url to fetch or perform operations on data to the backend
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8080" }),
+  baseQuery: baseQueryWithReauth,
 
   // Tag types will be used for cached data
   tagTypes: ["Data Form", "Migrant", "User"],
