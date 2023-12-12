2aa67e3a6d564e00df7023a98adb5807a6635034
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-12-12 17:36:15 +0000

    Modified dasboard footer

--- src/components/DashboardFooter.jsx
+++ src/components/DashboardFooter.jsx
@@ -1,5 +1,57 @@
-const DashboardFooter = () => {
-  return <h1>Footer</h1>;
+import { useLocation, useNavigate } from "react-router-dom";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import useAuth from "../hooks/useAuth";
+
+const DashFooter = () => {
+  const { Username, status } = useAuth();
+
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  const redirectToHome = () => navigate("/dash/welcome");
+
+  let redirectBtn = null;
+  if (pathname !== "/dash") {
+    redirectBtn = (
+      <button className="dash-footer" title="Home" onClick={redirectToHome}>
+        Home Icon
+        {/* <FontAwesomeIcon icon={faHouse} /> */}
+      </button>
+    );
+  }
+
+  const footerStyles = {
+    display: "flex",
+    position: "absolute",
+    bottom: 0,
+    color: "#fff",
+    width: "100vw",
+    padding: "2rem 2.5rem",
+    backgroundColor: "#000",
+  };
+
+  const content = (
+    <footer style={footerStyles}>
+      <div>
+        <h6>{Username} </h6>
+        <h6>{status} </h6>
+        {redirectBtn}
+      </div>
+      <div className="about-footer">
+        <h4>About Migrant Flow</h4>
+        <h6>About us</h6>
+        <h6>Our mission</h6>
+      </div>
+      <div className="contact-footer">
+        <h4>Contact us</h4>
+        <h6>Advertise</h6>
+        <h6>Whatsapp</h6>
+        <h6>Email</h6>
+      </div>
+    </footer>
+  );
+  return content;
 };
 
-export default DashboardFooter;
+export default DashFooter;
