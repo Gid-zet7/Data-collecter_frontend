3f200c467c01be8fe239f3cfbf87c4f48426af3e
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-12-15 05:28:40 +0000

    Modified data form to include a delete button so users can delete research forms/questionnaires

--- src/features/dataForms/DataForm.jsx
+++ src/features/dataForms/DataForm.jsx
@@ -1,10 +1,14 @@
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/prop-types */
-import { useGetDataFormsQuery } from "./dataFormsApiSlice";
+import {
+  useDeleteDataFormMutation,
+  useGetDataFormsQuery,
+} from "./dataFormsApiSlice";
 import { memo, useState, useEffect } from "react";
 import Button from "@mui/material/Button";
 import { useAddNewDataMutation } from "../researchData/DataApiSlice";
 import { useNavigate } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
 
 const DataForm = ({ dataformId }) => {
   // Destructure the dataform from result
@@ -16,20 +20,25 @@ const DataForm = ({ dataformId }) => {
 
   const [addNewData, { isSuccess, isError, error }] = useAddNewDataMutation();
 
+  const [deleteDataform, { isDelSuccess, isDelError, delerror }] =
+    useDeleteDataFormMutation();
+
   const [data, setData] = useState(
     dataform.questions.map((question) => {
       return { question: question.questionText, response: "" };
     })
   );
 
+  const { Username } = useAuth();
+
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (isSuccess) {
+    if (isSuccess || isDelSuccess) {
       setData("");
-      navigate("/dashboard");
+      navigate("/dashboard/data");
     }
-  }, [isSuccess, navigate]);
+  }, [isSuccess, isDelSuccess, navigate]);
 
   const onResponseChanged = (response, question, index) => {
     let newData = [...data];
@@ -46,7 +55,34 @@ const DataForm = ({ dataformId }) => {
     });
   };
 
-  const errClass = isError ? "errmsg" : "offscreen";
+  const onDeleteButtonClicked = async () => {
+    await deleteDataform({
+      id: dataformId,
+    });
+  };
+
+  let deleteButton;
+  if (dataform.user_id?.username === Username) {
+    deleteButton = (
+      <div>
+        <Button
+          variant="contained"
+          color="error"
+          style={{
+            fontSize: "14px",
+            maxWidth: "20rem",
+            // backgroundColor: "tomato",
+          }}
+          onClick={onDeleteButtonClicked}
+          // disabled={!canSave}
+        >
+          Delete
+        </Button>
+      </div>
+    );
+  }
+
+  const errClass = isError || isDelError ? "errmsg" : "offscreen";
 
   if (dataform) {
     // const handleEdit = () => navigate(`/dashboard/dataforms/${dataformId}`);
@@ -56,12 +92,15 @@ const DataForm = ({ dataformId }) => {
         <form
           className="data-form"
           style={{
-            border: "1px solid #000",
+            boxShadow:
+              "rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px",
             marginBottom: "35px",
             width: "clamp(15rem, 50vw, 60rem)",
           }}
         >
-          <p className={errClass}>{error?.data?.message}</p>
+          <p className={errClass}>
+            {(error?.data?.message || delerror?.data?.message) ?? ""}
+          </p>
           <div
             className="form_header"
             style={{
@@ -70,6 +109,7 @@ const DataForm = ({ dataformId }) => {
               borderRadius: "8px",
             }}
           >
+            <p>form by {dataform.user_id?.username}</p>
             <h1>Title: {dataform.form_title} </h1>
             <span>Description</span>
             <p>{dataform.form_desc} </p>
@@ -78,7 +118,15 @@ const DataForm = ({ dataformId }) => {
 
           {dataform.questions.map((question, i) => {
             return (
-              <div key={i} className={`question_container ${i}`}>
+              <div
+                key={i}
+                className={`question_container ${i}`}
+                style={{
+                  boxShadow:
+                    "rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px",
+                  padding: "1rem",
+                }}
+              >
                 <p>{question.questionText}:</p>
                 {question.options.map((option) => {
                   return (
@@ -116,18 +164,23 @@ const DataForm = ({ dataformId }) => {
               </div>
             );
           })}
-          <Button
-            variant="contained"
-            color="primary"
-            style={{ fontSize: "14px", maxWidth: "20rem" }}
-            onClick={onSubmitForm}
-            // disabled={!canSave}
-          >
-            Submit
-          </Button>
+          <div style={{ display: "flex" }}>
+            <Button
+              variant="contained"
+              color="primary"
+              style={{
+                fontSize: "14px",
+                maxWidth: "20rem",
+                marginRight: "35px",
+              }}
+              onClick={onSubmitForm}
+              // disabled={!canSave}
+            >
+              Submit
+            </Button>
+            {deleteButton}
+          </div>
         </form>
-
-        {/* <button onClick={handleEdit}>Edit</button> */}
       </>
     );
   } else return null;
